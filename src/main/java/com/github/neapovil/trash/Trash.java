ae968771f563dd1dc144a06ce9395d3019af8ea5
William <nearata@protonmail.com>
2022-11-17 15:34:59 +0100

    update dependencies
    * drop IF library, overkill

--- src/main/java/com/github/neapovil/trash/Trash.java
+++ src/main/java/com/github/neapovil/trash/Trash.java
@@ -1,78 +1,38 @@
 package com.github.neapovil.trash;
 
-import org.bukkit.Material;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.github.stefvanschie.inventoryframework.gui.GuiItem;
-import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
-import com.github.stefvanschie.inventoryframework.pane.PatternPane;
-import com.github.stefvanschie.inventoryframework.pane.util.Pattern;
-
 import dev.jorel.commandapi.CommandAPICommand;
+import net.kyori.adventure.text.Component;
 
-public final class Trash extends JavaPlugin
+public final class Trash extends JavaPlugin implements Listener
 {
     private static Trash instance;
+    private final NamespacedKey key = new NamespacedKey(this, "trash");
 
     @Override
     public void onEnable()
     {
         instance = this;
 
+        this.getServer().getPluginManager().registerEvents(this, this);
+
         new CommandAPICommand("trash")
                 .withPermission("trash.command.gui")
                 .executesPlayer((player, args) -> {
-                    final ChestGui gui = new ChestGui(1, "Trash");
-                    final Pattern pattern = new Pattern("111101111");
-                    final PatternPane pane = new PatternPane(0, 0, 9, 1, pattern);
-                    final GuiItem guiitem = new GuiItem(new ItemStack(Material.GRAY_STAINED_GLASS_PANE));
+                    final Inventory inventory = this.getServer().createInventory(player, 9, Component.text("Trash"));
 
-                    guiitem.setAction(event -> event.setCancelled(true));
+                    player.getPersistentDataContainer().set(this.key, PersistentDataType.INTEGER, inventory.hashCode());
 
-                    pane.bindItem('1', guiitem);
-
-                    gui.addPane(pane);
-
-                    gui.setOnBottomClick(event -> {
-                        if (!event.getClick().equals(ClickType.SHIFT_LEFT))
-                        {
-                            return;
-                        }
-
-                        if (event.getCurrentItem() == null)
-                        {
-                            return;
-                        }
-
-                        event.getCurrentItem().setAmount(0);
-                    });
-
-                    gui.setOnOutsideClick(event -> event.setCancelled(true));
-
-                    gui.setOnTopClick(event -> {
-                        if (event.getCursor() == null)
-                        {
-                            return;
-                        }
-
-                        if (event.getCurrentItem() != null)
-                        {
-                            return;
-                        }
-
-                        if (event.getClick().equals(ClickType.RIGHT))
-                        {
-                            event.getCursor().setAmount(event.getCursor().getAmount() - 1);
-                            event.setCancelled(true);
-                            return;
-                        }
-
-                        event.getCursor().setAmount(0);
-                    });
-
-                    gui.show(player);
+                    player.openInventory(inventory);
                 })
                 .register();
     }
@@ -85,5 +45,90 @@ public final class Trash extends JavaPlugin
     public static Trash getInstance()
     {
         return instance;
+    }
+
+    @EventHandler
+    private void inventoryClose(InventoryCloseEvent event)
+    {
+        if (this.isTrash((Player) event.getPlayer(), event.getInventory().hashCode()))
+        {
+            event.getPlayer().getPersistentDataContainer().remove(this.key);
+        }
+    }
+
+    @EventHandler
+    private void topClick(InventoryClickEvent event)
+    {
+        if (!this.isTrash((Player) event.getWhoClicked(), event.getInventory().hashCode()))
+        {
+            return;
+        }
+
+        if (event.getClickedInventory() == null)
+        {
+            return;
+        }
+
+        if (!event.getClickedInventory().equals(event.getView().getTopInventory()))
+        {
+            return;
+        }
+
+        if (event.getCursor() == null)
+        {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        if (event.isRightClick())
+        {
+            event.getCursor().subtract();
+        }
+
+        if (event.isLeftClick())
+        {
+            event.getCursor().setAmount(0);
+        }
+    }
+
+    @EventHandler
+    private void shiftClick(InventoryClickEvent event)
+    {
+        if (!this.isTrash((Player) event.getWhoClicked(), event.getInventory().hashCode()))
+        {
+            return;
+        }
+
+        if (!event.isShiftClick())
+        {
+            return;
+        }
+
+        if (event.getCurrentItem() == null)
+        {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        event.getCurrentItem().setAmount(0);
+    }
+
+    private boolean isTrash(Player player, int inventoryHashcode)
+    {
+        if (!player.getPersistentDataContainer().has(this.key))
+        {
+            return false;
+        }
+
+        final int hashcode = player.getPersistentDataContainer().get(this.key, PersistentDataType.INTEGER);
+
+        if (inventoryHashcode != hashcode)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
